6fff0c431ee5e42dcf76e28e7eb39bf04a9465df
Alex <orangesofter@users.noreply.github.com>
2022-05-22 18:13:10 +0300

    Добавлены зависимости Compose

--- app/src/main/java/ru/upsoft/gpspointer/presentation/MainActivity.kt
+++ app/src/main/java/ru/upsoft/gpspointer/presentation/MainActivity.kt
@@ -2,21 +2,23 @@ package ru.upsoft.gpspointer.presentation
 
 import android.Manifest
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.commit
 import by.kirich1409.viewbindingdelegate.viewBinding
-import dagger.hilt.android.AndroidEntryPoint
 import ru.upsoft.gpspointer.R
 import ru.upsoft.gpspointer.databinding.ActivityMainBinding
 import ru.upsoft.gpspointer.presentation.common.showErrorPermissionMessage
 import ru.upsoft.gpspointer.presentation.ui.features.mainFunctionality.MainFunctionalityFragment
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : ComponentActivity() {
 
-    private val viewBinding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
     private lateinit var requestLocation: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,22 +32,18 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     )
                     return@registerForActivityResult
                 }
-                /*if(isGooglePlayServicesAvailable())*/ commitMainFunctionalityFragment()
-                /*else showAlertMessageWithoutNegativeButton(
-                    getString(R.string.attention),
-                    getString(R.string.google_play_services_error),
-                    false,
-                    ::finish
-                )*/
+                setContent { RootScreen() }
             }
             requestLocation.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 
-    private fun commitMainFunctionalityFragment(){
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            add(R.id.fragmentContainer, MainFunctionalityFragment(), "mainFuncFrag")
+    @Preview
+    @Composable
+    fun RootScreen() {
+        Column {
+            Text("Мой compose text 1")
+            Text("А это следующий)")
         }
     }
 
